5aebf489e40db133cdf4f2cec000815ff1432478
Alexandra Osmakova <alexandra.osmakova@gmail.com>
2019-10-03 19:59:29 +0300

    feat: add simple task list

--- app/scripts/controllers/UserController.js
+++ app/scripts/controllers/UserController.js
@@ -7,28 +7,42 @@ app.controller("UserController", function UserController(
   $scope.weather = {
     currentTemp: 0,
     currentDescription: undefined
-  }
+  };
+  $scope.todoList;
   if ($scope.username) {
     $scope.username = sessionStorage.getItem("user");
     $scope.userCity = sessionStorage.getItem("city");
     $scope.token = sessionStorage.getItem("token");
-    var link = 'http://api.openweathermap.org/data/2.5/weather';
+    var link = "http://api.openweathermap.org/data/2.5/weather";
     $http
-      .get(`${link}?q=${$scope.userCity}&lang=ru&units=metric&appid=${$scope.token}`)
+      .get(
+        `${link}?q=${$scope.userCity}&lang=ru&units=metric&appid=${$scope.token}`
+      )
       .then(function(response) {
         $scope.weather = {
           currentTemp: String(response.data.main.temp),
           currentDescription: response.data.weather[0].description
-        }
+        };
         $scope.checkTempType();
+        $scope.getToDoList();
       });
   } else {
     $location.path("");
   }
 
   $scope.checkTempType = function() {
-    if($scope.weather.currentTemp[0] !== '-') {
-      $scope.weather.currentTemp = $scope.weather.currentTemp.replace (/^/,'+');
+    if ($scope.weather.currentTemp[0] !== "-") {
+      $scope.weather.currentTemp = $scope.weather.currentTemp.replace(/^/, "+");
     }
-  }
+  };
+
+  $scope.getToDoList = function() {
+    $http
+      .get(
+        "https://my-json-server.typicode.com/alexandra-osmakova/fake-db/posts/"
+      )
+      .then(function(response) {
+        $scope.todoList = response.data;
+      });
+  };
 });
